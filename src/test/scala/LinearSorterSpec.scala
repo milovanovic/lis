de9067b490c20a0e4998eb5eb771c27dc9a36340
milovanovic <barun@eecs.berkeley.edu>
2022-07-29 17:21:06 +0200

    Add all test cases for LIS-SR

--- src/test/scala/LinearSorterSpec.scala
+++ src/test/scala/LinearSorterSpec.scala
@@ -11,18 +11,19 @@ import scala.util.{Random}
 import scala.collection.mutable.ArrayBuffer
 
 
-class LinearSorterCNTsSpec extends FlatSpec with Matchers {
+class LinearSortersSpec extends FlatSpec with Matchers {
 
   behavior of "Linear streaming insertion sorters"
 
-  for (sorterSubType <- Seq("LIS_fixed", "LIS_FIFO", "LIS_input")) {
-   // for (sorterSubType <- Seq("LIS_FIFO")) {
+  for (sorterType <- Seq("LIS_CNT", "LIS_SR")) {
+    for (sorterSubType <- Seq("LIS_fixed", "LIS_FIFO", "LIS_input")) {
       for (sorterSize <- Seq(8, 16, 64)) {
         for(sortDir <- Seq(true, false)) {
-          it should f"work for UInt, sorter type $sorterSubType, sorter size = $sorterSize and parameter sortDir = $sortDir" ignore {//ignore {//in {
+          it should f"work for UInt, sorter type $sorterType, sorter subtype $sorterSubType, sorter size = $sorterSize and parameter sortDir = $sortDir" ignore {//ignore {//in {
             val params: LISParams[UInt] = LISParams(
               proto = UInt(16.W),
               LISsize = sorterSize,
+              LIStype = sorterType,
               LISsubType = sorterSubType,
               discardPos = if (sorterSubType == "LIS_fixed") Some(sorterSize/2) else None,
               rtcSize = false,
@@ -30,7 +31,7 @@ class LinearSorterCNTsSpec extends FlatSpec with Matchers {
             )
             val in = Seq.fill(params.LISsize)(Random.nextInt(1<<(params.proto.getWidth)).toDouble) // max is exclusive 0 is inclusive
             dsptools.Driver.execute(
-              () => new LinearSorterCNT(params),
+              () => new LinearSorter(params),
               Array(
               "--backend-name", "treadle", // "treadle", // "verilator",
               "--target-dir", s"test_run_dir/$sorterSubType-$sorterSize-UInt-$sortDir")
@@ -41,13 +42,16 @@ class LinearSorterCNTsSpec extends FlatSpec with Matchers {
         }
       }
     }
+  }
 
+  for (sorterType <- Seq("LIS_CNT", "LIS_SR")) {
     for (sorterSubType <- Seq("LIS_fixed", "LIS_FIFO", "LIS_input")) {
       for (sorterSize <- Seq(2, 16, 32)) {
         for (sortDir <- Seq(true, false)) {
-          it should f"work for SInt, sorter type $sorterSubType, sorter size = $sorterSize and sortDir = $sortDir" ignore {
+          it should f"work for SInt, sorter type $sorterType, sorter subtype $sorterSubType, sorter size = $sorterSize and sortDir = $sortDir" ignore {
             val params: LISParams[SInt] = LISParams(
               proto = SInt(16.W),
+              LIStype = sorterType,
               LISsize = sorterSize,
               LISsubType = sorterSubType,
               discardPos = if (sorterSubType == "LIS_fixed") Some(0) else None,
@@ -57,7 +61,7 @@ class LinearSorterCNTsSpec extends FlatSpec with Matchers {
             )
             val in = Seq.fill(params.LISsize)((Random.nextInt((1<<(params.proto.getWidth-1))*2) - (1<<(params.proto.getWidth-1))).toDouble)
             dsptools.Driver.execute(
-                () => new LinearSorterCNT(params),
+                () => new LinearSorter(params),
                 Array(
                 "--backend-name", "treadle", // "treadle", // "verilator",
                 "--target-dir", s"test_run_dir/$sorterSubType-$sorterSize-SInt-$sortDir")
@@ -68,14 +72,17 @@ class LinearSorterCNTsSpec extends FlatSpec with Matchers {
         }
       }
     }
+  }
 
+  for (sorterType <- Seq("LIS_CNT", "LIS_SR")) {
     for (sorterSubType <- Seq("LIS_fixed", "LIS_FIFO", "LIS_input")) {
       for (sorterSize <- Seq(2, 16, 32)) {
         for (sortDir <- Seq(true,false)) {
-          it should f"work for FixedPoint, sorter type $sorterSubType, sorter size = $sorterSize and sortDir = $sortDir" ignore {//in {
+          it should f"work for FixedPoint, sorter type $sorterType, sorter subtype $sorterSubType, sorter size = $sorterSize and sortDir = $sortDir" ignore {//in {
             val params: LISParams[FixedPoint] = LISParams(
               proto = FixedPoint(16.W, 8.BP),
               LISsize = sorterSize,
+              LIStype = sorterType,
               LISsubType = sorterSubType,
               discardPos = if (sorterSubType == "LIS_fixed") Some(sorterSize/2) else None,
               rtcSortDir = false,
@@ -84,7 +91,7 @@ class LinearSorterCNTsSpec extends FlatSpec with Matchers {
             )
             val in: Seq[Double] = Seq.fill(params.LISsize)((Random.nextDouble()*2-1) * ((1<<params.proto.getWidth - params.proto.binaryPoint.get-1)))
             dsptools.Driver.execute(
-                () => new LinearSorterCNT(params),
+                () => new LinearSorter(params),
                 Array(
                 "--backend-name", "treadle", // "treadle", // "verilator",
                 "--target-dir", s"test_run_dir/$sorterSubType-$sorterSize-FixedPoint-$sortDir")
@@ -95,14 +102,17 @@ class LinearSorterCNTsSpec extends FlatSpec with Matchers {
         }
       }
     }
+  }
 
+  for (sorterType <- Seq("LIS_CNT", "LIS_SR")) {
     for (sorterSubType <- Seq("LIS_fixed", "LIS_FIFO", "LIS_input")) {
       for (sorterSize <- Seq(2, 16, 32)) {
         for (sortDir <- Seq(true, false)) {
-          it should f"work for DspReal, sorter type $sorterSubType and  sorter size = $sorterSize and sortDir = $sortDir" ignore {//ignore {
+          it should f"work for DspReal, sorter type $sorterType, sorter subtype $sorterSubType and  sorter size = $sorterSize and sortDir = $sortDir" ignore {//ignore {
             val params: LISParams[DspReal] = LISParams(
               proto = DspReal(),
               LISsize = sorterSize,
+              LIStype = sorterType,
               LISsubType = sorterSubType,
               discardPos = if (sorterSubType == "LIS_fixed") Some(sorterSize/2) else None,
               rtcSize = false,
@@ -110,7 +120,7 @@ class LinearSorterCNTsSpec extends FlatSpec with Matchers {
             )
             val in = Seq.fill(params.LISsize)((Random.nextInt(Double.MaxValue.toInt) - Double.MaxValue.toInt).toDouble)
             dsptools.Driver.execute(
-                () => new LinearSorterCNT(params),
+                () => new LinearSorter(params),
                 Array(
                 "--backend-name", "treadle", // "treadle", // "verilator",
                 "--target-dir", s"test_run_dir/$sorterSubType-$sorterSize-DspReal-$sortDir")
@@ -121,37 +131,40 @@ class LinearSorterCNTsSpec extends FlatSpec with Matchers {
         }
       }
     }
+  }
 
-   // test run time configurability!
-   for (sorterSubType <- Seq("LIS_fixed", "LIS_FIFO", "LIS_input")) {
-      for (sorterSize <- Seq(8, 16, 32)) {
-        it should f"work for FixedPoint, sorter type $sorterSubType, compile sorter size = $sorterSize and rtcSize configurable sorter size" ignore {//in {
-          val params: LISParams[FixedPoint] = LISParams(
-            proto = FixedPoint(16.W, 8.BP),
-            LISsize = sorterSize,
-            LISsubType = sorterSubType,
-            discardPos = if (sorterSubType == "LIS_fixed") Some(0) else None,
-            rtcSize = true,
-            sortDir = true
-          )
-          val in: Seq[Double] = Seq.fill(params.LISsize)((Random.nextDouble()*2-1) * ((1<<params.proto.getWidth - params.proto.binaryPoint.get-1)))
-          dsptools.Driver.execute(
-              () => new LinearSorterCNT(params),
-              Array(
-              "--backend-name", "treadle", // "treadle", // "verilator",
-              "--target-dir", s"test_run_dir/$sorterSubType-$sorterSize-FixedPoint-rtcSizeSize")
-           ){ c =>
-                new LinearSorterTesterRunTime(c, in, 1)
-          } should be (true)
+  for (sorterType <- Seq("LIS_CNT", "LIS_SR")) {
+    // test run time configurability!
+    for (sorterSubType <- Seq("LIS_fixed", "LIS_FIFO", "LIS_input")) {
+        for (sorterSize <- Seq(8, 16, 32)) {
+          it should f"work for FixedPoint, sorter type $sorterType, sorter subtype $sorterSubType, compile sorter size = $sorterSize and rtcSize configurable sorter size" ignore {//in {
+            val params: LISParams[FixedPoint] = LISParams(
+              proto = FixedPoint(16.W, 8.BP),
+              LISsize = sorterSize,
+              LIStype = sorterType,
+              LISsubType = sorterSubType,
+              discardPos = if (sorterSubType == "LIS_fixed") Some(0) else None,
+              rtcSize = true,
+              sortDir = true
+            )
+            val in: Seq[Double] = Seq.fill(params.LISsize)((Random.nextDouble()*2-1) * ((1<<params.proto.getWidth - params.proto.binaryPoint.get-1)))
+            dsptools.Driver.execute(
+                () => new LinearSorter(params),
+                Array(
+                "--backend-name", "treadle", // "treadle", // "verilator",
+                "--target-dir", s"test_run_dir/$sorterSubType-$sorterSize-FixedPoint-rtcSizeSize")
+            ){ c =>
+                  new LinearSorterTesterRunTime(c, in, 1)
+            } should be (true)
+          }
         }
       }
     }
-
    // test rtcSize configurable sorting direction together with rtcSize configurable sorter size
-   for (sorterSubType <- Seq("LIS_fixed", "LIS_FIFO", "LIS_input")) {
-   //for (sorterSubType <- Seq("LIS_FIFO")) {
+  for (sorterType <- Seq("LIS_CNT", "LIS_SR")) {
+    for (sorterSubType <- Seq("LIS_fixed", "LIS_FIFO", "LIS_input")) {
       for (sorterSize <- Seq(4, 15, 32)) { // test power of 2 and non power of 2 sorter size
-        it should f"work for FixedPoint, sorter type $sorterSubType, compile sorter size = $sorterSize and rtcSize configurable sorting direction and sorter size" ignore {
+        it should f"work for FixedPoint, sorter type $sorterType, sorter subtype $sorterSubType, compile sorter size = $sorterSize and rtcSize configurable sorting direction and sorter size" ignore {
           val params: LISParams[FixedPoint] = LISParams(
             proto = FixedPoint(16.W, 8.BP),
             LISsize = sorterSize,
@@ -163,7 +176,7 @@ class LinearSorterCNTsSpec extends FlatSpec with Matchers {
           )
           val in: Seq[Double] = Seq.fill(params.LISsize)((Random.nextDouble()*2-1) * ((1<<params.proto.getWidth - params.proto.binaryPoint.get-1)))
           dsptools.Driver.execute(
-              () => new LinearSorterCNT(params),
+              () => new LinearSorter(params),
               Array(
               "--backend-name", "verilator", // "treadle", // "verilator",
               "--target-dir", s"test_run_dir/$sorterSubType-$sorterSize-FixedPoint-rtcSizeSize")
@@ -173,12 +186,12 @@ class LinearSorterCNTsSpec extends FlatSpec with Matchers {
         }
       }
     }
-
+  }
    // test flushData parameter
 //     for (sorterSubType <- Seq("LIS_FIFO", "LIS_fixed", "LIS_input")) {
 //       for (sorterSize <- Seq(2,16,32)) {
 //         for (sortDir <- Seq(true,false)) {
-//           it should f"work for FixedPoint, sorter type $sorterSubType, sorter size = $sorterSize, sortDir = $sortDir and included flushData parameter" ignore {
+//           it should f"work for FixedPoint, sorter subtype $sorterSubType, sorter size = $sorterSize, sortDir = $sortDir and included flushData parameter" ignore {
 //             val params: LISParams[FixedPoint] = LISParams(
 //               proto = FixedPoint(16.W, 8.BP),
 //               LISsize = sorterSize,
@@ -190,7 +203,7 @@ class LinearSorterCNTsSpec extends FlatSpec with Matchers {
 //             )
 //           val in: Seq[Double] = Seq.fill(params.LISsize)((Random.nextDouble()*2-1) * ((1<<params.proto.getWidth - params.proto.binaryPoint.get-1)))
 //             dsptools.Driver.execute(
-//                 () => new LinearSorterCNT(params),
+//                 () => new LinearSorter(params),
 //                 Array(
 //                 "--backend-name", "firrtl", // "treadle", // "verilator",
 //                 "--target-dir", s"test_run_dir/$sorterSubType-$sorterSize-FixedPoint")
@@ -201,14 +214,15 @@ class LinearSorterCNTsSpec extends FlatSpec with Matchers {
 //         }
 //       }
 //     }
-    
+  for (sorterType <- Seq("LIS_CNT")) {
     for (sorterSubType <- Seq("LIS_FIFO", "LIS_fixed", "LIS_input")) {
       for (sorterSize <- Seq(2, 4, 16, 25)) {
         for (sortDir <- Seq(true, false)) {
-          it should f"work for FixedPoint, sorter type $sorterSubType, sorter size = $sorterSize, sortDir = $sortDir and with lastIn/ lastOut" in {
+          it should f"work for FixedPoint, sorter type $sorterType, sorter subtype $sorterSubType, sorter size = $sorterSize, sortDir = $sortDir and with lastIn/ lastOut" in {
             val params: LISParams[FixedPoint] = LISParams(
               proto = FixedPoint(16.W, 8.BP),
               LISsize = sorterSize,
+              LIStype = sorterType,
               LISsubType = sorterSubType,
               discardPos = if (sorterSubType == "LIS_fixed") Some(sorterSize/2) else None,
               rtcSize = false,
@@ -217,9 +231,9 @@ class LinearSorterCNTsSpec extends FlatSpec with Matchers {
             )
           val in: Seq[Double] = Seq.fill(params.LISsize)((Random.nextDouble()*2-1) * ((1<<params.proto.getWidth - params.proto.binaryPoint.get-1)))
              dsptools.Driver.execute(
-                () => new LinearSorterCNT(params),
+                () => new LinearSorter(params),
                 Array(
-                "--backend-name", "treadle", // "treadle", // "verilator",
+                "--backend-name", "verilator", // "treadle", // "verilator",
                 "--target-dir", s"test_run_dir/$sorterSubType-$sorterSize-FixedPoint")
             ){ c =>
                   new LinearSorterTesterTestLastIn(c, in, 1)
@@ -228,5 +242,33 @@ class LinearSorterCNTsSpec extends FlatSpec with Matchers {
         }
       }
     }
-    
+  }
+
+  for (sorterSubType <- Seq("LIS_FIFO")) {
+    for (sorterSize <- Seq(2, 4, 16, 25)) {
+      for (sortDir <- Seq(true, false)) {
+        it should f"work for FixedPoint, sorter type LIS_SR, sorter subtype $sorterSubType, sorter size = $sorterSize, sortDir = $sortDir and with lastIn/ lastOut" in {
+          val params: LISParams[FixedPoint] = LISParams(
+            proto = FixedPoint(16.W, 8.BP),
+            LISsize = sorterSize,
+            LIStype = "LIS_SR",
+            LISsubType = sorterSubType,
+            discardPos = if (sorterSubType == "LIS_fixed") Some(sorterSize/2) else None,
+            rtcSize = false,
+            flushData = false,
+            sortDir = sortDir
+          )
+        val in: Seq[Double] = Seq.fill(params.LISsize)((Random.nextDouble()*2-1) * ((1<<params.proto.getWidth - params.proto.binaryPoint.get-1)))
+            dsptools.Driver.execute(
+              () => new LinearSorter(params),
+              Array(
+              "--backend-name", "verilator", // "treadle", // "verilator",
+              "--target-dir", s"test_run_dir/$sorterSubType-$sorterSize-FixedPoint")
+          ){ c =>
+                new LinearSorterTesterTestLastIn(c, in, 1)
+          } should be (true)
+        }
+      }
+    }
+  }
 }
