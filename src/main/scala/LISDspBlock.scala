b2f9f36ec8e4d741e6aac46b1cff430f0f9c5880
Vladimir Milovanovic <barun@eecs.berkeley.edu>
2023-07-05 13:15:35 +0200

    Better coding for control regs definition

--- src/main/scala/LISDspBlock.scala
+++ src/main/scala/LISDspBlock.scala
@@ -22,23 +22,17 @@ abstract class LISBlock[T <: Data: Real: BinaryRepresentation, D, U, E, O, B <: 
     with DspBlock[D, U, E, O, B]
     with HasCSR {
 
+  require(
+    params.rtcSize == true || params.rtcSortDir == true || params.flushData == true || params.useSorterEmpty == true || params.useSorterFull == true
+  )
+
   val streamNode = AXI4StreamIdentityNode()
 
   lazy val module = new LazyModuleImp(this) {
     val (in, _) = streamNode.in(0)
     val (out, _) = streamNode.out(0)
 
-    val lis = Module(new LinearSorterCNT(params))
-
-    // control registers
-    val sortDir = RegInit(true.B)
-    val flushData = RegInit(false.B)
-    val discardPos = RegInit(0.U(log2Ceil(params.LISsize).W))
-    val lisSize = RegInit(0.U(log2Ceil(params.LISsize).W))
-
-    // status registers
-    val sorterFull = RegInit(false.B)
-    val sorterEmpty = RegInit(true.B)
+    val lis = Module(new LinearSorter(params))
 
     // connect input
     lis.io.in.valid := in.valid
@@ -48,64 +42,78 @@ abstract class LISBlock[T <: Data: Real: BinaryRepresentation, D, U, E, O, B <: 
     out.bits.last := lis.io.lastOut
     in.ready := lis.io.out.ready
 
-    // connect control registers
-    if (params.rtcSortDir) {
-      lis.io.sortDir.get := sortDir
-    }
-    if (params.rtcSize == true) {
-      lis.io.lisSize.get := lisSize
-    }
-    if (params.flushData == true) {
-      lis.io.flushData.get := flushData
-    }
-    if (params.LISsubType == "LIS_input") {
-      lis.io.discardPos.get := discardPos
-    }
-
     // connect output
     out.valid := lis.io.out.valid
     lis.io.out.ready := out.ready
     out.bits.data := lis.io.out.bits.asUInt
 
-    // connect status registers
-    if (params.useSorterFull) {
-      sorterFull := lis.io.sorterFull.get
-    }
-    if (params.useSorterEmpty) {
-      sorterEmpty := lis.io.sorterEmpty.get
-    }
+    var commonFields = Seq[RegField]()
 
-    val fields = Seq(
-      // settable registers
-      RegField(
+    if (params.rtcSortDir) {
+      val sortDir = RegInit(true.B)
+      lis.io.sortDir.get := sortDir
+      commonFields = commonFields :+ RegField(
         1,
         sortDir,
         RegFieldDesc(
           name = "sortDir",
-          desc = "define sorting direction (`true` denotes ascending, `false` denotes descending sorting direction) "
+          desc = "define sorting direction (`true` denotes ascending, `false` denotes descending sorting direction"
         )
-      ),
-      RegField(
+      )
+    }
+
+    if (params.rtcSize) {
+      val lisSize = RegInit(0.U(log2Ceil(params.LISsize).W))
+      lis.io.lisSize.get := lisSize
+      commonFields = commonFields :+ RegField(
         log2Ceil(params.LISsize),
         lisSize,
         RegFieldDesc(name = "lisSize", desc = "contains lis size which is used for run time configurability control")
-      ),
-      RegField(1, flushData, RegFieldDesc(name = "flushData", desc = "trigger data flushing")),
-      RegField(
+      )
+    }
+
+    if (params.flushData == true) {
+      val flushData = RegInit(false.B)
+      commonFields =
+        commonFields :+ RegField(1, flushData, RegFieldDesc(name = "flushData", desc = "trigger data flushing"))
+      lis.io.flushData.get := flushData
+    }
+
+    if (params.LISsubType == "LIS_input") {
+      val discardPos = RegInit(0.U(log2Ceil(params.LISsize).W))
+      commonFields = commonFields :+ RegField(
         log2Ceil(params.LISsize),
         discardPos,
         RegFieldDesc(
           name = "discardPos",
-          desc = "defines position of the discarding element - used only if LIS_input scheme is used"
+          desc = "defines position of the discarding element - used only for LIS_input scheme"
         )
-      ),
-      // read-only status registers
-      RegField.r(1, sorterFull, RegFieldDesc(name = "sorterFull", desc = "indicates whether sorter is full or not")),
-      RegField.r(1, sorterEmpty, RegFieldDesc(name = "sorterEmpty", desc = "indicates whether sorter is empty or not"))
-    )
-    //define abract register map so it can be AXI4, Tilelink, APB, AHB
+      )
+      lis.io.discardPos.get := discardPos
+    }
+
+    if (params.useSorterFull) {
+      val sorterFull = RegInit(false.B)
+      commonFields = commonFields :+ RegField.r(
+        1,
+        sorterFull,
+        RegFieldDesc(name = "sorterFull", desc = "indicates whether sorter is full or not")
+      )
+      sorterFull := lis.io.sorterFull.get
+    }
+
+    if (params.useSorterEmpty) {
+      val sorterEmpty = RegInit(true.B)
+      commonFields = commonFields :+ RegField.r(
+        1,
+        sorterEmpty,
+        RegFieldDesc(name = "sorterEmpty", desc = "indicates whether sorter is empty or not")
+      )
+      sorterEmpty := lis.io.sorterEmpty.get
+    }
+
     regmap(
-      fields.zipWithIndex.map({
+      commonFields.zipWithIndex.map({
         case (f, i) =>
           i * beatBytes -> Seq(f)
       }): _*
@@ -160,7 +168,7 @@ object LISAXI4DspBlock extends App {
     proto = FixedPoint(16.W, 14.BP),
     LISsize = 8,
     LISsubType = "LIS_input",
-    rtcSize = false,
+    rtcSize = true,
     sortDir = true
   )
   val baseAddress = 0x500
@@ -179,7 +187,7 @@ object LISTLDspBlock extends App {
     proto = FixedPoint(16.W, 14.BP),
     LISsize = 8,
     LISsubType = "LIS_input",
-    rtcSize = false,
+    rtcSize = true,
     sortDir = true
   )
   val baseAddress = 0x500
